e7e82ad087837c0f543373308f00b0127332a9d1
Radek Holy <rholy@redhat.com>
2015-08-14 15:47:49 +0200

    Create the releasever testing repository only if releasever is tested
    The repository is specific to these tests and also it will be easier to create install root specific repositories later.
    
    Resolves: https://github.com/rholy/dnf-extra-tests/issues/12

--- features/environment.py
+++ features/environment.py
@@ -14,9 +14,8 @@
 
 """This module provides the test fixture common to all the features.
 
-Among other things, the fixture contains a testing RPM repository and
-the default configuration of DNF (including the backup of the previous
-configuration). The repository contains at least one package.
+Among other things, the fixture contains the default configuration of
+DNF (including the backup of the previous configuration).
 
 The :class:`behave.runner.Context` instance passed to the environmental
 controls and to the step implementations is expected to have following
@@ -26,10 +25,6 @@ attributes:
     A name of the DNF configuration file.
 :attr:`!backupfn` : :class:`str` | :data:`None`
     A name of the DNF configuration backup file.
-:attr:`!repoid` : :class:`str`
-    An ID of the testing repository.
-:attr:`!reponevras` : :class:`list[str]`
-    The NEVRAs of all the packages in the testing repository.
 
 """
 
@@ -39,13 +34,9 @@ from __future__ import unicode_literals
 
 import os
 import shutil
-import subprocess
 import tempfile
-import urllib
-import urlparse
 
-import createrepo_c
-import dnf.rpm
+import dnf
 
 
 def before_all(context):
@@ -53,20 +44,11 @@ def before_all(context):
 
     :param context: the context as described in the environment file
     :type context: behave.runner.Context
-    :raises exceptions.OSError: if the repository cannot be created or
-       if DNF cannot be configured
-    :raises shutil.Error: if the repository cannot be created
     :raises exceptions.IOError: if DNF cannot be configured
 
     """
     with dnf.Base() as base:
-        releasever = dnf.rpm.detect_releasever(base.conf.installroot)
         context.configfn = base.conf.config_file_path
-    repopardn = tempfile.mkdtemp()
-    repodn = os.path.join(repopardn, releasever)
-    shutil.copytree(
-        os.path.join(os.path.dirname(__file__), b'resources', b'repository'),
-        repodn)
     configbackup = tempfile.NamedTemporaryFile(
         'wb',
         prefix='{}.bak'.format(os.path.basename(context.configfn)),
@@ -75,25 +57,9 @@ def before_all(context):
     with open(context.configfn, 'rb') as configfile, configbackup:
         shutil.copyfileobj(configfile, configbackup)
     context.backupfn = configbackup.name
-    context.repoid = b'dnf-extra-tests'
-    # We need a slash at the end so that the urljoin below appends to the URL.
-    repoparurl = urllib.pathname2url(os.path.join(repopardn, b''))
-    # We need urljoin to avoid a quotation of the variable.
-    repourl = urlparse.urljoin(
-        urlparse.urlunsplit((b'file', b'', repoparurl, b'', b'')),
-        b'$RELEASEVER')
-    with open(context.configfn, 'wt') as configfile:
-        configfile.write(
-            b'[{}]\n'
-            b'baseurl={}\n'
-            b'metadata_expire=600\n'
-            .format(context.repoid, repourl))
-    metadata = createrepo_c.Metadata()
-    metadata.locate_and_load_xml(repodn)
-    context.reponevras = [metadata.get(key).nevra() for key in metadata.keys()]
+    open(context.configfn, 'wt').close()
     # Build DNF's cache.
     with dnf.Base() as base:
-        base.conf.substitutions['releasever'] = releasever
         base.read_all_repos()
         base.fill_sack()
 
@@ -101,20 +67,14 @@ def before_all(context):
 def after_all(context):
     """Do the cleanup that can be done at the very end.
 
-    The "dnf" executable must be available.
-
     :param context: the context as described in the environment file
     :type context: behave.runner.Context
-    :raises exceptions.OSError: if the executable cannot be executed or
-       if the DNF configuration backup cannot be removed
+    :raises exceptions.OSError: if the DNF configuration backup cannot
+       be removed
     :raises exceptions.IOError: if the original DNF configuration cannot
        be restored
 
     """
-    subprocess.call([
-        b'dnf', b'--quiet', b'--disablerepo=*',
-        b'--enablerepo={}'.format(context.repoid),
-        b'clean', b'metadata'])
     if context.backupfn:
         try:
             shutil.copyfile(context.backupfn, context.configfn)
